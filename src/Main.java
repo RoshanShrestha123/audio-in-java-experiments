d78f79ed929743ab9650367cd1cf60e0b4d8a9f0
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2023-02-14 17:14:05 +0545

    get the sample frequence from the streamline audio sample and display

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args){
 
-        Sound sound = new Sound();
-        sound.AudioStream();
+
 
         JFrame _frame = new JFrame("Playing audio on the background");
-        JButton _button = new JButton("Click me");
-        _frame.add(_button);
+        _frame.setSize(400, 500);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        _frame.setLayout(null);
-        _frame.setVisible(true);
 
+
+        Sound sound = new Sound(_frame);
+        sound.AudioStream();
+
+//        _frame.setLayout(null);
+        _frame.setVisible(true);
 
     }
 }
--- src/Sound.java
+++ src/Sound.java
@@ -1,6 +1,9 @@
 import javax.sound.sampled.*;
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Sound {
     int BUFFER_SIZE = 1024;
@@ -9,6 +12,13 @@ public class Sound {
     private Clip clip;
     private int bytesPerFrame;
     private int numBytes;
+    private JFrame frame;
+
+
+    Sound(JFrame _frame){
+        this.frame = _frame;
+    }
+
 
     public void AudioStream() {
         File file = new File("file_example_WAV_1MG.wav");
@@ -18,29 +28,54 @@ public class Sound {
             audioInputStream = AudioSystem.getAudioInputStream(file);
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-            clip.start();
 
-
-
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+        } catch (UnsupportedAudioFileException | IOException e) {
+            throw new RuntimeException(e);
+        } catch (LineUnavailableException e) {
             throw new RuntimeException(e);
         }
 
         bytesPerFrame = audioInputStream.getFormat().getFrameSize();
-        System.out.println(bytesPerFrame);
-        numBytes = BUFFER_SIZE * bytesPerFrame;
-        byte[] audioBytes = new byte[numBytes];
-        int byteRead = 0;
+
+        byte[] audioBytes;
         try {
-            byteRead = audioInputStream.read(audioBytes);
-            System.out.println("Total bytes per frame:---" + bytesPerFrame);
-            System.out.println("Buffer size:---" + BUFFER_SIZE);
-            System.out.println("current byte reading:---" + byteRead);
+            audioBytes = new byte[audioInputStream.available()];
+            audioInputStream.read(audioBytes);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        float sampleRate = audioInputStream.getFormat().getSampleRate();
+        int sampleSizeInBits = audioInputStream.getFormat().getSampleSizeInBits();
+        int channel = audioInputStream.getFormat().getChannels();
 
+        short[] samples = new short[audioBytes.length / 2];
+
+        System.out.println("Sample frequency[rate]: " + sampleRate + " Hertz(HZ)");
+        System.out.println("Sample size[bits]: " + sampleSizeInBits);
+        System.out.println("Total channels: " + channel);
+        System.out.println("Audio in bytes: " + audioBytes.length);
+
+        ByteBuffer.wrap(audioBytes)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .asShortBuffer()
+                .get(samples);
+
+        float SIGNED_INT_16_BIT_MAX_VALUE = 32768.0f;
+        for (int i = 0; i < samples.length; i++) {
+            System.out.print(samples[i]/SIGNED_INT_16_BIT_MAX_VALUE +" ");
+        }
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Sample frequency[rate]: " + sampleRate + " Hertz(HZ)");
+
+        JLabel label2 = new JLabel("Sample size[bits]: " + sampleSizeInBits);
+
+        panel.add(label);
+        panel.add(label2);
+
+        this.frame.add(panel);
+        clip.start();
     }
 
 }
